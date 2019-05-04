ab5428fe4886fdc349520caf0e2cfdcfc949b6ae
RomainP <romain.petit@student.uclouvain.be>
2019-05-04 15:04:25 +0200

    import de la version finale du calcul cons-voy

--- cv.c
+++ cv.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define CONSONNE 1
+#define VOYELLE 0
 
-u_int8_t max(u_int8_t a, u_int8_t b){
-  if (a <= b){
-    return (b);
-  }
-  else {
-    return (a);
-  }
-}
-
-
-u_int8_t compteur (char *mdp){
+u_int8_t compteur (char *mdp,int consouvoye){
   u_int8_t voy = 0 ;
-  u_int8_t compteur = 0;
+  u_int8_t cons = 0;
   while (*mdp){
     switch (*mdp){
       case 'a': case'e' : case'i' : case'o' : case'u' : case'y':
@@ -21,13 +13,25 @@ u_int8_t compteur (char *mdp){
         break;
     }
     *mdp++;
-    compteur ++;
+    cons ++;
+}if(consouvoye==0){
+  return voy;
 }
-  return(max(voy , compteur-voy));
+  return (cons-voy) ;
 }
+//u_int8_t max(u_int8_t a, u_int8_t b){
+  //if (a <= b){
+    //return (b);
+  //}
+  //else {
+    //return (a);
+  //}
+//}
+//int main (int argc, char *argv[]){
+  //char *c = argv[1];
+  //char* mot= "eeebc";
+  //printf("%d\n", compteur(mot,1) );
+  //return 0;
+//}
 
-int main (int argc, char *argv[]){
-  char *c = argv[1];  
-  printf("%d\n", compteur(c) );
-  return 0;
-}
+
