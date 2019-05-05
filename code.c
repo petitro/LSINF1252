8f919fc7ce6cea103cc800a451848f25d4c9d838
RomainP <romain.petit@student.uclouvain.be>
2019-05-05 15:47:43 +0200

    import de la premiere version de la fct reverse

--- code.c
+++ code.c
@@ -28,7 +28,7 @@ struct candidat {
   struct candidat *next ;
 }t_candidat;
 
-struct candidat *candidatHead;
+struct candidat *candidatHead;//sert pour repérer le début de la liste ?
 struct candidat *candidatH
 struct fichierIn *head ;
 struct fichierIn *courant ;
@@ -58,10 +58,26 @@ void *readFile(){
   printf("%s\n", "entrer dans readFile" );
   return(NULL);
 }
-
-void *reverse(){
-  printf("Entrée dans reverse\n");
-  return(NULL);
+void* reverse(void){
+	printf("Entrée dans reverse\n");
+	while(true){
+		sem_wait(&semHFull);
+		pthread_mutex_lock(&mutH);
+		uint8_t *hash = bufH[positionH];
+		bufH[positionH]=NULL;
+		positionH=positionH-1;
+		pthread_mutex_unlock(&mutH);
+		sem_post(&semHEmpty);
+		
+		char mot[16] = malloc(sizeof(char)*16);
+		bool=reversehash(hash,mot,16);
+		sem_wait(&semMDPEmpty);
+		pthread_mutex_lock(&mutMDP);
+		buffMDP[positionMDP]=mot;
+		positionMDP=positionMDP+1;
+		pthread_mutex_unlock(&mutMDP);
+		sem_post(&semMDPFull);
+	}
 }
 
 void *count (){
